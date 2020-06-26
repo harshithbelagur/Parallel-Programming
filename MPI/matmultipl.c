58a34b7f2fb9562f3ca2b4d29c550123b5013813
Harshith Belagur <h.belagur1980@gmail.com>
2020-06-26 12:25:01 +0530

    Add files via upload

---
+++ MPI/matmultipl.c
@@ -0,0 +1,71 @@
+//Author - Harshith Belagur
+//Date - 14/06/2020
+//Matrix Multiplication on MPI
+
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <mpi.h>
+#define N 4
+
+void print_results(char *prompt, int a[N][N])
+{
+    int i, j;
+
+    printf ("\n\n%s\n", prompt);
+    for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                    printf(" %d", a[i][j]);
+            }
+            printf ("\n");
+    }
+    printf ("\n\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int i, j, k, rank, size, tag = 99, blksz, sum = 0;
+    int a[N][N]={{1,2,3,4},{5,6,7,8},{9,1,2,3},{4,5,6,7,}};
+    int b[N][N]={{1,2,3,4},{5,6,7,8},{9,1,2,3},{4,5,6,7,}};
+    int c[N][N];
+    int aa[N],cc[N];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    //scatter rows of first matrix to different processes     
+    MPI_Scatter(a, N*N/size, MPI_INT, aa, N*N/size, MPI_INT,0,MPI_COMM_WORLD);
+
+    //broadcast second matrix to all processes
+    MPI_Bcast(b, N*N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+          //perform vector multiplication by all processes
+          for (i = 0; i < N; i++)
+            {
+                    for (j = 0; j < N; j++)
+                    {
+                            sum = sum + aa[j] * b[j][i];               
+                    }
+                    cc[i] = sum;
+                    sum = 0;
+            }
+
+    MPI_Gather(cc, N*N/size, MPI_INT, c, N*N/size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);        
+    MPI_Finalize();
+    if (rank == 0)                         
+        print_results("C = ", c);
+}
+
+// Output - 
+// C = 
+//  54 37 47 57
+//  1 2 3 4
+//  44 41 56 71
+//  9 1 2 3
